41f8ddf6cf50095aa78ec26aa13b9c4abd2e381e
Mariam Hiza <mariamhhiza@egmail.com>
2024-03-22 11:44:59 +0300

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `niulize` will be documented in this file
+
+## 1.0.0 - 201X-XX-XX
+
+- initial release
---
+++ src/Niulize.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hizamariam\Niulize;
+
+class Niulize
+{
+   public function askqn()
+   {
+        return "What do you want to know about us?";
+   }
+}
